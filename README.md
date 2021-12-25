aaf2242185ae3da67976462e37927319ef671650
vachtung-gigabidze <dmitry.zyadik@gmail.com>
2021-12-26 00:20:58 +0500

    add awesomefont, db.json

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Evodius
+A simple calculation of fuel consumption, and then how it goes.
--- src/Main.elm
+++ src/Main.elm
@@ -6,11 +6,21 @@ import Element as E
 import Element.Background as EBA
 import Element.Font as F
 import Url
+import FontAwesome.Attributes as Icon
+import FontAwesome.Brands as Icon
+import FontAwesome.Icon as Icon exposing (Icon)
+import FontAwesome.Layering as Icon
+import FontAwesome.Solid as Icon
+import FontAwesome.Styles as Icon
+import FontAwesome.Svg as SvgIcon
+import FontAwesome.Transforms as Icon
+
+
 
 type alias Flags =
     {}
 
--- The main function of the ELm program
+-- The main function of the Elm program
 main : Program Flags Model Msg
 main =
     Browser.application
@@ -35,7 +45,16 @@ subscriptions : Model -> Sub Msg
 subscriptions _ =
     Sub.none
 
+type alias Car = 
+  { gn : String,
+    model : String
+  }
 
+carNumbers : List Car
+carNumbers =  [ { gn = "103", model = "спецтранспорт"}
+                , { gn = "328", model =""}
+                , {gn = "107", model = ""} 
+              ]
 ---------------------------------------- MODEL/UPDATE ----------------------------------------
 -- Data central to the application. This application has no data.
 type alias Model =
@@ -55,38 +74,52 @@ update msg model =
         UrlChanged url ->
             ( model, Cmd.none )
 
+header : E.Element msg
+header = 
+  E.row [ E.width E.fill, EBA.color (E.rgb255 222 1 22), E.spacing 20, E.padding 20 ]
+        [
+           E.el [] (E.html (Icon.viewIcon Icon.truck))
+           , E.el [] (E.text "Калькулятор")
+           , E.el [] (E.text "v0.0.1")
+        ]
+
+content =
+       E.row
+          [ E.centerX
+          , E.centerY
+          , E.spacing 42
+          , E.padding 100
+          ]
+          [E.el
+            [ E.centerX
+            , E.centerY
+            , F.bold
+            , F.size 42
+            ] renderCarNumbers]
+
+bodyRender = [ E.layout
+            [ E.width E.fill
+            , E.height E.fill
+            
+            , EBA.color <| E.rgb255 176 192 200
+            , E.inFront <| header
+            ]
+            <| E.column 
+                [E.width E.fill]
+                [E.html <| Icon.css, header, content]
+
+            
+        ]
+
+carRender: Car -> E.Element msg
+carRender c =  E.el [] <| E.text c.gn        
+renderCarNumbers =
+  E.column [E.width E.fill] <| List.map carRender carNumbers 
 ---------------------------------------- VIEW ----------------------------------------
 
 
-view : Model -> Browser.Document Msg
+-- view : Model -> Browser.Document Msg
 view model =
     { title = "Расход топлива"
-    , body =
-        [ E.layout
-            [ E.width E.fill
-            , E.height E.fill
-            , EBA.color <| E.rgb255 176 192 200
-            ]
-            <| E.row
-                [ E.centerX
-                , E.centerY
-                , E.spacing 42
-                ]
-                [ E.image
-                    [ E.centerX
-                    , E.centerY
-                    , E.width <| E.px 150
-                    ]
-                    { src = "./assets/images/fission.svg"
-                    , description = "fission"
-                    }
-                , E.el
-                    [ E.centerX
-                    , E.centerY
-                    , F.bold
-                    , F.size 42
-                    ]
-                    <| E.text "All Set Up!"
-                ]
-        ]
+    , body = bodyRender       
     }
